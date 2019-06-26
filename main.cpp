81eed7f25a194e8001400aac91d9f419ebcaf314
Your Name <you@example.com>
2019-06-27 00:20:06 +0300

    first commit

---
+++ main.cpp
@@ -0,0 +1,42 @@
+#include <tins/tins.h>
+#include <iostream>
+#include <nlohmann/json.hpp>
+#include <vector>
+
+using std::cout;
+using std::endl;
+using json = nlohmann::json;
+
+using namespace Tins;
+
+bool callback(const PDU& pdu) {
+    DNS dns = pdu.rfind_pdu<RawPDU>().to<DNS>();
+    std::vector<std::string> ips;
+    json j;
+
+    for (const auto& answer : dns.answers()){
+        ips.push_back(answer.data());
+        j["domain"] = answer.dname();
+        j["ttl"] = answer.ttl();
+    }
+    j["ips"] = ips;
+    cout << j << endl;
+    j.clear();
+    return true;
+}
+
+int main(int argc, char* argv[]) {
+    if(argc != 2) {
+        cout << "Usage: " <<* argv << " <interface>" << endl;
+        return 1;
+    }
+    // Sniff on the provided interface in promiscuos mode
+    SnifferConfiguration config;
+    config.set_promisc_mode(true);
+    // Only capture udp packets sent to port 53
+    config.set_filter("udp and src port 53");
+    Sniffer sniffer(argv[1], config);
+
+    // Start the capture
+    sniffer.sniff_loop(callback);
+}

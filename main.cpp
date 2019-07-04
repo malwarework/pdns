07792629d88cf41a42f2767b8e5adb934d3d17e8
Your Name <you@example.com>
2019-07-05 00:40:10 +0300

    modify cmakelist

--- main.cpp
+++ main.cpp
@@ -34,18 +34,17 @@ void timer_start(std::function<void(std::vector<Candidate>&)> func, unsigned int
     PeriodicListPrunning filter2;
     std::thread([func, filter2, interval, cron]() {
         if(cron){
+            func(L);
+#ifdef DEBUG
+            std::this_thread::sleep_for(std::chrono::seconds(interval));
+#else
             tm timeout_tm={0};
             timeout_tm.tm_hour = 0;
             timeout_tm.tm_min = 0;
             timeout_tm.tm_sec = 0;
             timeout_tm.tm_isdst = -1;
             time_t timeout_time_t=mktime(&timeout_tm);
-
-            func(L);
-#ifdef DEBUG
-            std::this_thread::sleep_for(std::chrono::seconds(interval));
-#elif
-            std::chrono::system_clock::time_point timeout_tp=
+            std::chrono::system_clock::time_point timeout_tp =
                     std::chrono::system_clock::from_time_t(timeout_time_t);
             std::this_thread::sleep_until(timeout_tp);
             std::this_thread::sleep_for(std::chrono::hours(interval));
@@ -56,7 +55,7 @@ void timer_start(std::function<void(std::vector<Candidate>&)> func, unsigned int
             func(L);
 #ifdef DEBUG
             std::this_thread::sleep_for(std::chrono::seconds(interval));
-#elif
+#else
             std::this_thread::sleep_for(std::chrono::hours(interval));
 #endif
         }
@@ -67,30 +66,31 @@ void timer_start(std::function<void(std::vector<Candidate>&)> func, unsigned int
 void F2a(std::vector<Candidate>& _L)
 {
     L_mutex.lock();
-    cout << endl;
     for (std::vector<Candidate>::iterator value=L.begin();value!=L.end();){
         std::set<IP_TYPE> networks;
-        for (auto ip : value->r){
+        for (auto ip : value->r)
+        {
             std::vector<std::string> results;
             boost::algorithm::split(results, ip, boost::algorithm::is_any_of("."));
             results[2] = results[3] = '0';
             networks.insert(boost::algorithm::join(results, "."));
         }
-        if ((value->q > 100) && (value->g.size() < 3) && ((value->r.size() <= 5) || ((float)(networks.size() / value->r.size()) <= 0.5))){
+
+        if ((value->q > 100) && (value->g.size() < 3) && ((value->r.size() <= 5) || ((float)(networks.size() / value->r.size()) <= 0.5)))
+        {
             ++value;
         }
-        else{
+        else
+        {
             value = L.erase(value);
         }
     }
     L_mutex.unlock();
 }
 
+
 void converttojson(std::vector<Candidate>& _L)
 {
-#ifdef KAFKA
-    KafkaConnector kafka;
-#endif
     std::vector<json> jv;
     L_mutex.lock();
     for (Candidate value : L)
@@ -109,35 +109,35 @@ void converttojson(std::vector<Candidate>& _L)
 #endif
         jv.push_back(j);
     }
+#ifdef KAFKA
+    KafkaConnector kafka;
     kafka.push(jv);
+#endif
+    L.clear();
     L_mutex.unlock();
 }
 
 
-std::set<IP_TYPE> resolveDomain(std::string domain){
-    // The sender
+std::set<IP_TYPE> resolveDomain(std::string domain, std::string nameserver="8.8.8.8")
+{
     PacketSender sender;
     std::set<IP_TYPE> ips;
-    // The DNS request
-    IP pkt = IP("8.8.8.8") / UDP(53, 1337) / DNS();
-    // Add the query
+    IP pkt = IP(nameserver) / UDP(53, 1337) / DNS();
     pkt.rfind_pdu<DNS>().add_query({ domain, DNS::A, DNS::IN });
-    // We want the query to be resolverd recursively
     pkt.rfind_pdu<DNS>().recursion_desired(1);
-
-    // Send and receive the response.
     std::unique_ptr<PDU> response(sender.send_recv(pkt));
-    // Did we receive anything?
-    if (response) {
-        // Interpret the response
+    if (response)
+    {
         DNS dns = response->rfind_pdu<RawPDU>().to<DNS>();
-        // Print responses
-        for (const auto &record : dns.answers()) {
-            if (record.query_type() == DNS::CNAME){
+        for (const auto &record : dns.answers())
+        {
+            if (record.query_type() == DNS::CNAME)
+            {
                 std::set<IP_TYPE> _ips = resolveDomain(record.data());
                 ips.insert(_ips.begin(), _ips.end());
             }
-            if(record.query_type() == DNS::A){
+            if(record.query_type() == DNS::A)
+            {
                 ips.insert(record.data());
             }
         }
@@ -146,7 +146,8 @@ std::set<IP_TYPE> resolveDomain(std::string domain){
 }
 
 
-bool callback(const PDU& pdu) {
+bool callback(const PDU& pdu)
+{
     DNS dns = pdu.rfind_pdu<RawPDU>().to<DNS>();
     Packet packet = (Packet)pdu;
     std::set<std::string> ips;
@@ -155,22 +156,26 @@ bool callback(const PDU& pdu) {
 
     TrafficeVolumeReduction tvr;
 
-    for (const auto& answer : dns.answers()){
+    for (const auto& answer : dns.answers())
+    {
         if (answer.query_type() == DNS::A){
             _dns.domain = answer.dname();
             trim_left_if(_dns.domain, boost::is_any_of("www."));
             _dns.ttl = answer.ttl();
-            try{
+            try
+            {
                 _dns.ips.insert(answer.data());
             }
-            catch(Tins::invalid_address){
+            catch(Tins::invalid_address)
+            {
 #ifdef ERROR
                 cout << "error IP:" <<  answer.data() << endl;
 #endif
                 continue;
             }
         }
-        if (answer.query_type() == DNS::CNAME){
+        if (answer.query_type() == DNS::CNAME)
+        {
             _dns.domain = answer.dname();
             trim_left_if(_dns.domain, boost::is_any_of("www."));
             _dns.ttl = answer.ttl();
@@ -178,7 +183,8 @@ bool callback(const PDU& pdu) {
             _dns.ips.insert(_ips.begin(), _ips.end());
         }
     }
-    if(tvr.F1(_dns)){
+    if(tvr.F1(_dns))
+    {
         PeriodicListPrunning f2;
         L_mutex.lock();
         f2.push(L, _dns);
@@ -188,12 +194,13 @@ bool callback(const PDU& pdu) {
 }
 
 
+int main(int argc, char* argv[])
+{
+    boost::property_tree::ptree pt;
 
-int main(int argc, char* argv[]) {
     options_description desc("Allowed options");
     desc.add_options()
             ("help,h", "produce help message")
-            ("interface,i", value<string>(), "set interface")
             ("config,c", value<string>()->default_value("passivedns.conf"), "config file path")
     ;
 
@@ -215,35 +222,15 @@ int main(int argc, char* argv[]) {
     {
         configfile = vm["config"].as<string>();
     }
-
-    string interface;
-    /*In case if interface*/
-    if(vm.count("interface"))
-    {
-        interface = vm["interface"].as<string>();
-    }
-    else
-    {
-        cout << "Please specify interface" << endl << "Use prefix -h" << endl;
-        return 1;
-    }
-
-    boost::property_tree::ptree pt;
     boost::property_tree::ini_parser::read_ini(configfile, pt);
-    int upload_hour = stoi(pt.get<std::string>("Global.UPLOAD_HOUR"));
-    int cron_time = stoi(pt.get<std::string>("Global.CRON_TIME"));
+    string interface = pt.get<std::string>("Global.interface");
 
-/*    if(argc != 2) {
-        #ifdef DEBUG
-            cout << "Usage: " <<* argv << " <interface>" << endl;
-        #endif
-        return 1;
-    }
-*/
 #ifdef DEBUG
     timer_start(F2a, 300);
     timer_start(converttojson, 900, false);
-#elif
+#else
+    int upload_hour = stoi(pt.get<std::string>("Global.UPLOAD_HOUR"));
+    int cron_time = stoi(pt.get<std::string>("Global.CRON_TIME"));
     timer_start(F2a, cron_time);
     timer_start(converttojson, upload_hour, true);
 #endif

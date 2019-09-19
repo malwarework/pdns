a939a67ded404a07f12613c827b256de3c5bfb72
Yury Sidarenka <suv@oac.gov.by>
2019-09-19 17:29:51 +0300

    remove error

--- src/PeriodicListPrunning.cpp
+++ src/PeriodicListPrunning.cpp
@@ -29,7 +29,7 @@ void PeriodicListPrunning::push(std::vector<Candidate> &L, DomainInfo domain){
             std::vector<IP_TYPE> union_ips;
             std::sort(old_ips.begin(), old_ips.end());
             std::sort(new_ips.begin(), new_ips.end());
-            std::set_union(old_ips.begin(), old_ips.end(), new_ips.begin(), new_ips.end(), std:back_inserter(union_ips));
+            std::set_union(old_ips.begin(), old_ips.end(), new_ips.begin(), new_ips.end(), std::back_inserter(union_ips));
             int lenips = union_ips.size() - new_ips.size();
             if (lenips > 0)
             {
